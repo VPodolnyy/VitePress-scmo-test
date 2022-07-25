d692041464ca538ff249c882c2e669843f63364a
Vladislav <vladislavpodolnyy@yandex.ru>
2022-07-25 14:30:10 +0300

    FEAT Тестовое отображение функционала

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'SCMO Help',
+    description: 'VitePress for scmo',
+    themeConfig: {
+        sidebar: [
+            {
+                items: [
+                    { text: 'Home', link: '/' },
+                    { text: 'page2', link: '/page2' },
+                    { text: 'page3', link: '/page3' }
+                ]
+            }
+        ]
+    }
+}
